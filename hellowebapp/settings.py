06f0612bec277192f5e2de3390873c94c42ddf09
Bill Phan <billphan25@gmail.com>
2017-08-03 08:33:27 -0700

    Added 404 errors, requirements.txt, and testing.

--- hellowebapp/settings.py
+++ hellowebapp/settings.py
@@ -9,12 +9,10 @@ https://docs.djangoproject.com/en/1.11/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/1.11/ref/settings/
 """
-
 import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
@@ -24,12 +22,10 @@ SECRET_KEY = '1j9dl2^nb-9b1#jx97jpm&h)9=n!l(!aot)d(&#ph3=nd8e-=&'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
-
-ALLOWED_HOSTS = []
-
+# TODO - UPDATE ALLOWED HOSTS BEFORE LAUNCH
+ALLOWED_HOSTS = ['*']
 
 # Application definition
-
 INSTALLED_APPS = [
     'collection', # added collection app
     'django.contrib.admin',
@@ -102,7 +98,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/1.11/topics/i18n/
 
@@ -115,7 +110,6 @@ USE_I18N = True
 USE_L10N = True
 
 USE_TZ = True
-
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
