1adf211d34b23f96a6f86dbd7ef4190aaef91d35
Bill Phan <billphan25@gmail.com>
2017-08-04 07:03:23 -0700

    Added contact form, facing context dictionary error.

--- collection/forms.py
+++ collection/forms.py
@@ -1,7 +1,19 @@
 from django.forms import ModelForm
+from django import forms
 from collection.models import Thing
 
 class ThingForm(ModelForm):
     class Meta:
         model = Thing
         fields = ('name', 'description',)
+
+class ContactForm(forms.Form):
+    contact_name = forms.CharField(required=True)
+    contact_email = forms.EmailField(required=True)
+    content = forms.CharField(required=True, widget=forms.Textarea)
+
+    def __init__(self, *args, **kwargs):
+        super(ContactForm, self).__init__(*args, **kwargs)
+        self.fields['contact_name'].label = "Your name:"
+        self.fields['contact_email'].label = "your email:"
+        self.fields['content'].label = "What do you want to say?"
