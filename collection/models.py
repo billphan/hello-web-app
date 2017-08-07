8bb66eaf2800a9e536524fc3ee28e64de90ae747
Bill Phan <billphan25@gmail.com>
2017-08-07 11:15:21 -0700

    Added social media model.

--- collection/models.py
+++ collection/models.py
@@ -6,3 +6,17 @@ class Thing(models.Model):
     description = models.TextField()
     slug = models.SlugField(unique=True)
     user = models.OneToOneField(User, blank=True, null=True)
+
+class Social(models.Model):
+    SOCIAL_TYPES = (
+        ('twitter', 'Twitter'),
+        ('facebook', 'Facebook'),
+        ('pinterest', 'Pinterest'),
+        ('instagram', 'Instagram'),
+    )
+    network = models.CharField(max_length=255, choices=SOCIAL_TYPES)
+    username = models.CharField(max_length=255)
+    thing = models.ForeignKey(Thing, related_name="social_accounts")
+
+    class Meta:
+        verbose_name_plural = "Social Media Links"
