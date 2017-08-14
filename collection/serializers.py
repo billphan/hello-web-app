87735d76c8a4ae35c94e0dd2d3f07cdad09cafe1
Bill Phan <billphan25@gmail.com>
2017-08-14 09:49:50 -0700

    Added serializers file, translates to JSON.

---
+++ collection/serializers.py
@@ -0,0 +1,7 @@
+from rest_framework import serializers
+from collection.models import Thing
+
+class ThingSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Thing
+        fields = ('name', 'description', 'slug',)
